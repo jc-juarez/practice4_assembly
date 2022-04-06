286217e1e3f8f42f825b0b96961292df88d91267
jc-juarez <goarp_jc@hotmail.com>
2022-04-06 23:10:02 +0000

    All Files

--- act2_4.s
+++ act2_4.s
@@ -1,16 +1,21 @@
 	.data
 	.balign 4
 format: .asciz " %d"
-format2: .asciz " %c"
+@format2: .asciz " %c"
 num1: .int 0
 num2: .int 0
 result: .int 0
 op: .byte 'a'
 input1: .asciz "Enter the first operand:\n"
-inputO: .asciz "Enter the operation to be performed (+, -, *, /):\n"
+inputO: .asciz "Enter the operation to be performed (+ is 1, - is 2, * is 3, / is 4):\n"
 input2: .asciz "Enter the second operand:\n"
-output: .asciz "\nThe result of %d %c %d is: %d\n"
-outTest: .asciz "The operator is %c\n"
+outputAdd: .asciz "\nThe result of %d + %d is: %d\n"
+outputSub: .asciz "\nThe result of %d - %d is: %d\n"
+outputMult: .asciz "\nThe result of %d * %d is: %d\n"
+outputDiv: .asciz "\nThe result of %d / %d is: %d\n"
+@outTest: .asciz "The operator is %c\n"
+
+output1: .asciz "\nThe result of sum is: %d\n"
 
 .text
 .global main
@@ -30,14 +35,9 @@ main:
 	ldr r0, =inputO
 	bl printf
 
-	ldr r0, =format2
+	ldr r0, =format
 	ldr r1, =op
 	bl scanf
-
-	ldr r0, =outTest
-	ldr r1, =op
-	ldr r1, [r1]
-	bl printf
 
 	ldr r0, =input2
 	bl printf
@@ -46,37 +46,108 @@ main:
 	ldr r1, =num2
 	bl scanf
 
+	ldr r3, =op
+	ldr r3, [r3]
+
+	cmp r3, #1
+	beq addition
+
+	cmp r3, #2
+	beq substraction
+
+	cmp r3, #3
+	beq multiplication
+
+	cmp r3, #4
+	beq division
+
+	b endResult
+
+addition:
 	ldr r1, =num1
 	ldr r1, [r1]
 	ldr r2, =num2
 	ldr r2, [r2]
-	ldr r3, =op
+	add r0, r1, r2
+
+	ldr r1, =result
+	str r0, [r1]
+
+	ldr r0, =outputAdd
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
+	ldr r3, =result
 	ldr r3, [r3]
+	bl printf
 
-	cmp r3, #0x2B
-	addeq r0, r1, r2
+	b endResult
 
-	cmp r3, #0x2D
-	subeq r0, r1, r2
+substraction:
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
+	sub r0, r1, r2
 
-	cmp r3, #0x2A
-	muleq r0, r1, r2
+	ldr r1, =result
+	str r0, [r1]
 
-	cmp r3, #0x2F
+	ldr r0, =outputSub
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
+	ldr r3, =result
+	ldr r3, [r3]
+	bl printf
+
+	b endResult
+
+multiplication:
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
+	mul r0, r1, r2
+
+	ldr r1, =result
+	str r0, [r1]
+
+	ldr r0, =outputMult
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
+	ldr r3, =result
+	ldr r3, [r3]
+	bl printf
+
+	b endResult
+
+division:
+	ldr r1, =num1
+	ldr r1, [r1]
+	ldr r2, =num2
+	ldr r2, [r2]
 	sdiveq r0, r1, r2
 
 	ldr r1, =result
 	str r0, [r1]
 
+	ldr r0, =outputDiv
 	ldr r1, =num1
 	ldr r1, [r1]
 	ldr r2, =num2
 	ldr r2, [r2]
-	ldr r3, =op
+	ldr r3, =result
 	ldr r3, [r3]
-	ldr r4, =result
-	ldr r4, [r2]
-	ldr r0, =output
 	bl printf
+
+	b endResult
+
+endResult:
+	
 
 	pop {ip, pc}
